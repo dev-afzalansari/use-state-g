5f944f3149b6fa132e4fa232a5d39ebb26c6daeb
afzal ansari <dev.afzalansari@gmail.com>
2022-10-15 23:07:54 +0545

    add comments for explaination

--- src/createHook.js
+++ src/createHook.js
@@ -5,6 +5,7 @@ export default function createHook() {
   const env = process.env.NODE_ENV;
   const state = useStore.state;
 
+  // setter returns a setState fn for passed key
   const setter = (sliceKey) => {
     if (typeof sliceKey === "undefined" && env !== "production")
       throw new Error(
@@ -24,6 +25,7 @@ export default function createHook() {
     return setState;
   };
 
+  // to set the initial state without causing an update
   const init = (key, value) => {
     if (
       typeof key === "undefined" ||
@@ -37,10 +39,21 @@ export default function createHook() {
     if (!slice && typeof value !== "undefined") {
       let obj = {};
       obj[key] = value;
+      /*
+      react-tivity's set method's second argument defaults to true 
+      when false it only changes the state without causing an update
+      to the subscribed components and is only needed if you wanna solve
+      this setstate-in-render problem basically when you try to update a
+      component while rendering another component react throws this error
+      https://github.com/facebook/react/issues/18178. To solve the problem
+      of setting the initial state while rendering parent component this 
+      trick solves the issue.
+      */
       state.set(obj, false);
     }
   };
 
+  // hook
   const useState = (key, value) => {
     if (!key && env !== "production")
       throw new Error(
